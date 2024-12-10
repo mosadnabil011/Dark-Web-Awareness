09ba2b592b55b5d01177067684e231eba64018b1
Mosad ABOKamer <mosadnabil011@gmail.com>
2024-12-11 01:03:12 +0200

    create project

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('header nav ul');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
